cd42cc561c4ae34a1d0c5f6daf5d6c5c7053cfde
Ashish Khairnar <ashish20171990@gmail.com>
2018-11-18 12:30:03 -0500

    Update one_liners.sh

--- one_liners.sh
+++ one_liners.sh
@@ -3,3 +3,7 @@ while read ip; do echo "$ip -       " `dig +answer -x $ip | grep SOA`; done < IP
 while read ip; do echo "$ip -       " `dig +answer -x $ip | grep 'SOA\|PTR'`; done < IP.txt > Output_PTR.txt
 
 while read ip; do echo "$ip -       " `whois $ip | grep CIDR\|NetName`; done < IP.txt > Output_WHOIS.txt
+
+**Cron:**
+_Every hour_
+0 1 * * * /usr/bin/python /home/abc.py
