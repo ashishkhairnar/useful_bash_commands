bdcad7e8356000010415c4640466e52f0bbab396
Ashish Khairnar <ashish20171990@gmail.com>
2019-03-02 01:32:09 +0800

    Delete LinuxPrivEscEnumeration.sh

--- LinuxPrivEscEnumeration.sh
+++
@@ -1,8 +0,0 @@
-*Sticky bit -*
-#Look for sticky bits in the file system - SUID/GUID
-   - find / -perm -u=s -type f 2>/dev/null
-   - ls -l /bin/su
-      -rw*s*r-xr-x 1 root root 40128 May 17  2012 /bin/su
-      
-    s indicates "s" or SUID bit is set.
-
